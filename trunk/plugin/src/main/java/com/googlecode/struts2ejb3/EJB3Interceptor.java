a4294971a016497be19eff04eca325be62039b27
schneidh <schneidh@129a98bc-373e-0410-88c4-c5d2dd3a2bbe>
2007-11-06 01:16:09 +0000

    initial checkin

---
+++ trunk/plugin/src/main/java/com/googlecode/struts2ejb3/EJB3Interceptor.java
@@ -0,0 +1,32 @@
+package com.googlecode.struts2ejb3;
+
+import java.lang.reflect.Field;
+
+import javax.naming.InitialContext;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+/**
+ * Looks for any fields annotated with the InjectEJB annotation and injects the
+ * given EJB into that field.
+ */
+public class EJB3Interceptor extends AbstractInterceptor implements Interceptor {
+
+	public String intercept(ActionInvocation actionInvocation) throws Exception {
+		Object action = actionInvocation.getAction();
+		for (Field f : action.getClass().getDeclaredFields()) {
+			if (f.isAnnotationPresent(InjectEJB.class)) {
+				InitialContext ic = new InitialContext();
+				String serviceName = f.getType().getName();
+				Object service = ic.lookup(serviceName);
+				boolean wasAccessible = f.isAccessible();
+				f.setAccessible(true);
+				f.set(action, f.getType().cast(service));
+				f.setAccessible(wasAccessible);
+			}
+		}
+		return actionInvocation.invoke();
+	}
+}
