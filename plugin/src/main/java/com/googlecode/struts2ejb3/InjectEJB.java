cf654628b11bb5c5a8955b99445a66060313c9ff
schneidh <schneidh@129a98bc-373e-0410-88c4-c5d2dd3a2bbe>
2007-11-06 01:16:09 +0000

    initial checkin

---
+++ plugin/src/main/java/com/googlecode/struts2ejb3/InjectEJB.java
@@ -0,0 +1,11 @@
+package com.googlecode.struts2ejb3;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.FIELD })
+public @interface InjectEJB {
+}
